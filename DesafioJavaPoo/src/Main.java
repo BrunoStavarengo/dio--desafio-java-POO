0d5efb8f3bfd8ce5b4077c7910aa3730ed15bd7f
BrunoStavarengo <brunostavarengo12@gmail.com>
2023-01-26 15:07:24 -0300

    interligando as classes de dominio

--- DesafioJavaPoo/src/Main.java
+++ DesafioJavaPoo/src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -22,9 +24,39 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
+        /*
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+        */
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Bruno");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Bruno: " + dev1.getConteudosInscritos());
+        dev1.progredir();
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("Conteudos Concluidos Bruno: " + dev1.getConteudosConcluidos());
+        System.out.println("Conteudos Inscritos Bruno: " + dev1.getConteudosInscritos());
+        System.out.println("XP: " + dev1.calculaTotalXp());
+        System.out.println("--------------------------------------------------------------");
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Lucas");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Lucas: " + dev2.getConteudosInscritos());
+        dev2.progredir();
+        System.out.println("Conteudos Concluidos Lucas: " + dev2.getConteudosConcluidos());
+        System.out.println("Conteudos Inscritos Lucas: " + dev2.getConteudosInscritos());
+        System.out.println("XP: " + dev2.calculaTotalXp());
 
     }
 }
