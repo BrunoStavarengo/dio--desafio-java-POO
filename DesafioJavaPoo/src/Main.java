ebde8378d44f31d9b14f78b23a0e4662386e51ae
Bruno <brunostavarengo12@gmail.com>
2023-01-27 14:13:29 -0300

    Incrementando o codigo

--- DesafioJavaPoo/src/Main.java
+++ DesafioJavaPoo/src/Main.java
@@ -7,40 +7,83 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main (String[] args){
+        Curso curso = new Curso();
         Curso curso1 = new Curso();
         Curso curso2 = new Curso();
+        Curso curso3 = new Curso();
+        Curso curso4 = new Curso();
+        Curso curso5 = new Curso();
 
-        curso1.setTitulo("Java");
-        curso1.setDescricao("Descrição curso de java");
-        curso1.setCargaHoraria(8);
 
-        curso2.setTitulo("Js");
-        curso2.setDescricao("Descrição curso de js");
+        curso.setTitulo("Java basico");
+        curso.setDescricao("Descrição curso de java basico");
+        curso.setCargaHoraria(8);
+
+        curso1.setTitulo("Java POO");
+        curso1.setDescricao("Descrição curso de java POO");
+        curso1.setCargaHoraria(6);
+
+        curso2.setTitulo("Spring boot");
+        curso2.setDescricao("Descrição curso de Spring boot+");
         curso2.setCargaHoraria(4);
 
+        curso3.setTitulo("HTML");
+        curso3.setDescricao("Descrição curso de HTML");
+        curso3.setCargaHoraria(8);
+
+        curso4.setTitulo("CSS");
+        curso4.setDescricao("Descrição curso de CSS");
+        curso4.setCargaHoraria(6);
+
+        curso5.setTitulo("Java Script");
+        curso5.setDescricao("Descrição curso de java script");
+        curso5.setCargaHoraria(4);
+
         Mentoria mentoria = new Mentoria();
+        Mentoria mentoria1 = new Mentoria();
+        Mentoria mentoria2 = new Mentoria();
+        Mentoria mentoria3 = new Mentoria();
 
         mentoria.setTitulo("Mentoria Java");
         mentoria.setDescricao("Descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        /*
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
-        */
+        mentoria1.setTitulo("Mentoria Java POO");
+        mentoria1.setDescricao("Descrição mentoria java POO");
+        mentoria1.setData(LocalDate.now());
+
+        mentoria2.setTitulo("Mentoria HTML");
+        mentoria2.setDescricao("Descrição mentoria HTML");
+        mentoria2.setData(LocalDate.now());
+
+        mentoria3.setTitulo("Mentoria CSS + JS");
+        mentoria3.setDescricao("Descrição mentoria CSS+JS");
+        mentoria3.setData(LocalDate.now());
 
         Bootcamp bootcamp = new Bootcamp();
-        bootcamp.setNome("Bootcamp Java Developer");
-        bootcamp.setDescricao("Descrição do Bootcamp Java Developer");
+        Bootcamp bootcamp1 = new Bootcamp();
+
+        bootcamp.setNome("Bootcamp Back-end");
+        bootcamp.setDescricao("Descrição do Bootcamp de Back-end");
+        bootcamp.getConteudos().add(curso);
         bootcamp.getConteudos().add(curso1);
         bootcamp.getConteudos().add(curso2);
         bootcamp.getConteudos().add(mentoria);
+        bootcamp.getConteudos().add(mentoria1);
+
+        bootcamp1.setNome("Bootcamp Front-end");
+        bootcamp1.setDescricao("Descrição do Bootcamp de Back-end");
+        bootcamp1.getConteudos().add(curso3);
+        bootcamp1.getConteudos().add(curso4);
+        bootcamp1.getConteudos().add(curso5);
+        bootcamp1.getConteudos().add(mentoria2);
+        bootcamp1.getConteudos().add(mentoria3);
 
         Dev dev1 = new Dev();
         dev1.setNome("Bruno");
         dev1.inscreverBootcamp(bootcamp);
-        System.out.println("Conteudos Inscritos Bruno: " + dev1.getConteudosInscritos());
+        System.out.println();
+        System.out.println("Conteudos Inscritos Bruno: "  + dev1.getConteudosInscritos());
         dev1.progredir();
         dev1.progredir();
         dev1.progredir();
@@ -51,12 +94,17 @@ public class Main {
 
         Dev dev2 = new Dev();
         dev2.setNome("Lucas");
-        dev2.inscreverBootcamp(bootcamp);
+        dev2.inscreverBootcamp(bootcamp1);
         System.out.println("Conteudos Inscritos Lucas: " + dev2.getConteudosInscritos());
+        dev2.progredir();
+        dev2.progredir();
+        dev2.progredir();
+        dev2.progredir();
         dev2.progredir();
         System.out.println("Conteudos Concluidos Lucas: " + dev2.getConteudosConcluidos());
         System.out.println("Conteudos Inscritos Lucas: " + dev2.getConteudosInscritos());
         System.out.println("XP: " + dev2.calculaTotalXp());
+
 
     }
 }
